7d0f1aba49253b86d06dcd1b8aaf21925c7ff221
juan <juanan_rp@hotmail.co.uk>
2018-09-21 12:16:12 +0100

    final submission

--- src/main/scala/GildedRoseKata/GoldenMaster.scala
+++ src/main/scala/GildedRoseKata/GoldenMaster.scala
@@ -17,20 +17,73 @@ class GoldenMaster(val items: Array[Item]) {
     }
   }
 
-  private def processNormalItems(currentItem: Int) = {
-    if (notASpecialItem(currentItem)) {
-      if (items(currentItem).quality > 0) {
-        items(currentItem).quality = decreaseQuality(currentItem)
+  private def processConcertTickets(currentItem: Int): Unit = {
+    if (items(currentItem).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+      if (qualityLessThanFifty(currentItem)) {
+        increaseQuality(currentItem)
+
+        if (items(currentItem).bestBeforeDate < 11) {
+          processItemWithLessThan50Quality(currentItem)
+        }
+        if (items(currentItem).bestBeforeDate < 6) {
+          processItemWithLessThan50Quality(currentItem)
+        }
       }
     }
   }
 
-  private def processAgedBrie(currentItem: Int) = {
-    if (items(currentItem).name.equals("Aged Brie")) {
-      if (items(currentItem).quality < 50) {
-        items(currentItem).quality = increaseQuality(currentItem)
+  private def processItemWithLessThan50Quality(currentItem: Int) = {
+    if (qualityLessThanFifty(currentItem)) {
+      increaseQuality(currentItem)
+    }
+  }
+
+  private def processItemsOutOfDate(currentItem: Int) = {
+    if (items(currentItem).bestBeforeDate < 0) {
+      if (items(currentItem).name.equals("Aged Brie")) {
+        processItemWithLessThan50Quality(currentItem)
+      } else {
+        if (items(currentItem).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+          items(currentItem).quality = removeAllQuality(currentItem)
+        } else {
+          if (qualityIsOver0(currentItem)) {
+              items(currentItem).quality = decreaseQuality(currentItem)
+          }
+        }
       }
     }
+  }
+
+  private def qualityLessThanFifty(currentItem: Int) = {
+    items(currentItem).quality < 50
+  }
+
+  private def reduceBestBeforeDate(currentItem: Int) = {
+    items(currentItem).bestBeforeDate = items(currentItem).bestBeforeDate - 1
+  }
+
+  private def removeAllQuality(currentItem: Int) = {
+    items(currentItem).quality - items(currentItem).quality
+  }
+
+  private def decreaseQuality(currentItem: Int) = {
+    items(currentItem).quality - 1
+  }
+
+  private def increaseQuality(currentItem: Int) = {
+    items(currentItem).quality =  items(currentItem).quality + 1
+  }
+
+  private def processNormalItems(currentItem: Int) = {
+    if (notASpecialItem(currentItem) && qualityIsOver0(currentItem)) items(currentItem).quality = decreaseQuality(currentItem)
+  }
+
+  private def qualityIsOver0(currentItem: Int) = {
+    items(currentItem).quality > 0
+  }
+
+  private def processAgedBrie(currentItem: Int) = {
+    if (items(currentItem).name.equals("Aged Brie") && qualityLessThanFifty(currentItem)) increaseQuality(currentItem)
   }
 
   private def notASpecialItem(currentItem: Int) = {
@@ -39,69 +92,6 @@ class GoldenMaster(val items: Array[Item]) {
 
   private def notALegendaryItem(i: Int) = {
     !items(i).name.equals("Sulfuras, Hand of Ragnaros")
-  }
-
-  private def processConcertTickets(i: Int): Unit = {
-    if (items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-      if (items(i).quality < 50) {
-        items(i).quality = increaseQuality(i)
-      }
-    }
-
-    if (items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-      if (items(i).quality < 50) {
-        if (items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-          if (items(i).bestBeforeDate < 11) {
-            processItemWithLessThan50Quality(i)
-          }
-          if (items(i).bestBeforeDate < 6) {
-            processItemWithLessThan50Quality(i)
-          }
-        }
-      }
-    }
-  }
-
-  private def processItemWithLessThan50Quality(i: Int) = {
-    if (items(i).quality < 50) {
-      items(i).quality = increaseQuality(i)
-    }
-  }
-
-  private def processItemsOutOfDate(i: Int) = {
-    if (items(i).bestBeforeDate < 0) {
-      if (items(i).name.equals("Aged Brie")) {
-        processItemWithLessThan50Quality(i)
-      } else {
-        if (items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-          items(i).quality = removeAllQuality(i)
-        } else {
-          if (items(i).quality > 0) {
-            if (items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-
-            } else {
-              items(i).quality = decreaseQuality(i)
-            }
-          }
-        }
-      }
-    }
-  }
-
-  private def reduceBestBeforeDate(i: Int) = {
-    items(i).bestBeforeDate = items(i).bestBeforeDate - 1
-  }
-
-  private def removeAllQuality(i: Int) = {
-    items(i).quality - items(i).quality
-  }
-
-  private def decreaseQuality(i: Int) = {
-    items(i).quality - 1
-  }
-
-  private def increaseQuality(i: Int) = {
-    items(i).quality + 1
   }
 }
 
