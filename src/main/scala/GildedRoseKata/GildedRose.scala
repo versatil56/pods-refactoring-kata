303323f4c35760dc92c6f355d9155690a81619e1
Patrick White <patrick.white@digital.hmrc.gov.uk>
2018-09-21 11:52:41 +0100

    Day 2 reArranged Code

--- src/main/scala/GildedRoseKata/GildedRose.scala
+++ src/main/scala/GildedRoseKata/GildedRose.scala
@@ -2,6 +2,42 @@ package GildedRoseKata
 
 class GildedRose(val items: Array[Item]) {
 
+  val agedBrie = "Aged Brie"
+  val backstage = "Backstage passes to a TAFKAL80ETC concert"
+  val sulphuras = "Sulfuras, Hand of Ragnaros"
+
+  def updateQuality(): Unit = {
+    items.foreach { it =>
+      changeSellIn(it)
+      it.name match {
+        case `backstage` =>
+          qualityPlusOne(it)
+
+          if (it.sellIn < 10)
+            qualityPlusOne(it)
+
+
+          if (it.sellIn < 5)
+            qualityPlusOne(it)
+
+          if (it.sellIn < 0)
+            it.quality = 0
+
+        case `agedBrie` =>
+          qualityPlusOne(it)
+          if (it.sellIn < 0)
+            qualityPlusOne(it)
+
+        case `sulphuras` => ()
+
+        case _ =>
+          qualityMinusOne(it)
+          if (it.sellIn < 0)
+            qualityMinusOne(it)
+      }
+    }
+  }
+
   private def qualityPlusOne(it: Item): Unit = {
     if (it.quality < 50) {
       it.quality = it.quality + 1
@@ -15,44 +51,8 @@ class GildedRose(val items: Array[Item]) {
   }
 
   private def qualityMinusOne(it: Item): Unit = {
-        if (it.quality > 0)
-            it.quality = it.quality - 1
-  }
-
-  val agedBrie = "Aged Brie"
-  val backstage = "Backstage passes to a TAFKAL80ETC concert"
-  val sulphuras = "Sulfuras, Hand of Ragnaros"
-
-  def updateQuality(): Unit = {
-    items.foreach { it =>
-      changeSellIn(it)
-      it.name match {
-        case `backstage` =>
-              qualityPlusOne(it)
-
-              if (it.sellIn < 10)
-                qualityPlusOne(it)
-
-
-              if (it.sellIn < 5)
-                qualityPlusOne(it)
-
-              if (it.sellIn < 0)
-                it.quality = 0
-
-        case `agedBrie` =>
-            qualityPlusOne(it)
-            if (it.sellIn < 0)
-              qualityPlusOne(it)
-
-        case `sulphuras` => ()
-
-        case _ =>
-          qualityMinusOne(it)
-          if (it.sellIn < 0)
-            qualityMinusOne(it)
-      }
-    }
+    if (it.quality > 0)
+      it.quality = it.quality - 1
   }
 
 }
