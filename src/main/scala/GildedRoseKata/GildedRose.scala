6ce12ec3ab09ed2df1118cb3b37a9cb22733daa8
Patrick White <patrick.white@digital.hmrc.gov.uk>
2018-09-21 11:49:51 +0100

    Day 2 new & improved

--- src/main/scala/GildedRoseKata/GildedRose.scala
+++ src/main/scala/GildedRoseKata/GildedRose.scala
@@ -2,13 +2,10 @@ package GildedRoseKata
 
 class GildedRose(val items: Array[Item]) {
 
-  private def changeQuality(i: Item, increment: Int): Unit = {
-  val newQuality = if ((i.quality + increment) >= 0 && (i.quality + increment)  <= 50) {
-      i.quality + increment
-    } else {
-      i.quality
+  private def qualityPlusOne(it: Item): Unit = {
+    if (it.quality < 50) {
+      it.quality = it.quality + 1
     }
-    i.quality = newQuality
   }
 
   private def changeSellIn(it: Item): Unit = {
@@ -17,79 +14,46 @@ class GildedRose(val items: Array[Item]) {
     }
   }
 
-  def updateQuality() {
-    val agedBrie = "Aged Brie"
-    val backstage = "Backstage passes to a TAFKAL80ETC concert"
-    val sulphuras = "Sulfuras, Hand of Ragnaros"
-    val conj = "Conjured"
-    items.foreach { it =>
-      (it.name, it.sellIn) match {
-        case (`sulphuras`, _) => it.quality
-        case (`backstage`, s) if s < 0 => 0
-        case (`backstage`, s) if s <= 5 => changeQuality(it, 3)
-        case (`backstage`, s) if s > 5 && s <=10 => changeQuality(it, 2)
-        case (`backstage`, s) if s > 10 => changeQuality(it, 1)
-        case (`agedBrie`, _) => changeQuality(it, 1)
-        case (`conj`, s) if s > 0 => changeQuality(it, -2)
-        case (`conj`, s) => changeQuality(it, -4)
-        case (_, s) if s > 0 => changeQuality(it, -1)
-        case (_, s) if s <= 0 => changeQuality(it, -2)
-      }
-
-      changeSellIn(it)
-      println( "Item:" + it.toString)
-    }
-    //
-    //   for (i <- 0 until items.length) {
-    //      if (!items(i).name.equals("Aged Brie")
-    //        && !items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-    //        if (items(i).quality > 0) {
-    //          if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-    //            items(i).quality = items(i).quality - 1
-    //          }
-    //        }
-    //      } else {
-    //        if (items(i).quality < 50) {
-    //          items(i).quality = items(i).quality + 1
-    //
-    //          if (items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-    //            if (items(i).sellIn < 11) {
-    //              if (items(i).quality < 50) {
-    //                items(i).quality = items(i).quality + 1
-    //              }
-    //            }
-    //
-    //            if (items(i).sellIn < 6) {
-    //              if (items(i).quality < 50) {
-    //                items(i).quality = items(i).quality + 1
-    //              }
-    //            }
-    //          }
-    //        }
-    //      }
-    //
-    //      if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-    //        items(i).sellIn = items(i).sellIn - 1
-    //      }
-    //
-    //      if (items(i).sellIn < 0) {
-    //        if (!items(i).name.equals("Aged Brie")) {
-    //          if (!items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-    //            if (items(i).quality > 0) {
-    //              if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-    //                items(i).quality = items(i).quality - 1
-    //              }
-    //            }
-    //          } else {
-    //            items(i).quality = items(i).quality - items(i).quality
-    //          }
-    //        } else {
-    //          if (items(i).quality < 50) {
-    //            items(i).quality = items(i).quality + 1
-    //          }
-    //        }
-    //      }
-    //    }
+  private def qualityMinusOne(it: Item): Unit = {
+        if (it.quality > 0)
+            it.quality = it.quality - 1
   }
+
+  val agedBrie = "Aged Brie"
+  val backstage = "Backstage passes to a TAFKAL80ETC concert"
+  val sulphuras = "Sulfuras, Hand of Ragnaros"
+
+  def updateQuality(): Unit = {
+    items.foreach { it =>
+      changeSellIn(it)
+      it.name match {
+        case `backstage` =>
+              qualityPlusOne(it)
+
+              if (it.sellIn < 10)
+                qualityPlusOne(it)
+
+
+              if (it.sellIn < 5)
+                qualityPlusOne(it)
+
+              if (it.sellIn < 0)
+                it.quality = 0
+
+        case `agedBrie` =>
+            qualityPlusOne(it)
+            if (it.sellIn < 0)
+              qualityPlusOne(it)
+
+        case `sulphuras` => ()
+
+        case _ =>
+          qualityMinusOne(it)
+          if (it.sellIn < 0)
+            qualityMinusOne(it)
+      }
+    }
+  }
+
 }
 
